acd6aea22edd9d4298feb602191ab74b1e92c00e
rutrut6969 <rutledgeisaac6969@gmail.com>
2020-05-06 19:18:38 -0500

    Finished for today, gonna finish up tomorrow

--- server.js
+++ server.js
@@ -2,12 +2,26 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => {
+const usersRouter = require('./users/userRouter');
+const postsRouter = require('./posts/postRouter');
+
+server.use(express.json());
+server.use('/api/users', logger, usersRouter);
+server.use('/api/posts', logger, postsRouter);
+
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  // request method, request url, and a timestamp
+  const today = Date.now();
+  console.log(
+    `Method: ${req.method}, URL: ${req.baseurl}${req.url}, Time: [${today}]`
+  );
+  next();
+}
 
 module.exports = server;
