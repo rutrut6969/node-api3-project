520b0843ab74112b1f5945985d2521e00648a387
rutrut6969 <rutledgeisaac6969@gmail.com>
2020-05-07 12:19:01 -0500

    Fixed dynamic ports

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // code away!
 const server = require('./server');
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
   console.log(`\n* Server Running on Port: ${port} *\n`);
